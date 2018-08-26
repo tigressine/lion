6fdc2f2d11158a115bc3b0001ada0b261657b489
Tiger <tgsachse@gmail.com>
2018-08-26 02:08:20 -0400

    initial work on help menu

--- source/plugins/help_menu.py
+++ source/plugins/help_menu.py
@@ -1,11 +1,21 @@
 """Still garbage, will revisit."""
 COMMAND = "help"
+HELP_HEADER = "Available commands for Lion:"
+HELP_FOOTER = "Get detailed help for any command with `!help command_name`."
+HELP_LIST = (
+    ("!tuc", "display the total user count for this server."),
+    ("!poll", "create a new poll."),
+    ("!gar", "see the status of UCF parking garages."),
+    ("!help", "view this menu."),
+)
+
 
 async def command_help_menu(client, message):
-    response = "```"
-    response += "Available commands for Lion\n"
-    response += "!usercount\n"
-    response += "   show how many users are on this server"
-    response += "```"
+    """"""
+    response = HELP_HEADER + "\n"
+    for command, description in HELP_LIST:
+        response += "**{0}**\n".format(command)
+        response += "   {0}\n".format(description)
+    response += "\n" + HELP_FOOTER
 
     await client.send_message(message.channel, response)
