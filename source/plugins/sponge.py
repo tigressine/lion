936cce138cc4d7abf6ba0f5737157f74a825ca9a
Tiger Sachse <tiger@sachse.io>
2018-08-27 17:25:59 -0400

    finished sponge

--- source/plugins/sponge.py
+++ source/plugins/sponge.py
@@ -1,6 +1,10 @@
-""""""
+"""Generate a sassy SpOnGeBoB meme with a provided message.
+
+Written by Tiger Sachse.
+"""
 import re
 import random
+import textwrap
 from PIL import Image, ImageDraw, ImageFont
 
 COMMAND = "spong"
@@ -12,8 +16,9 @@ FONT_FILE = "/usr/share/fonts/truetype/msttcorefonts/Arial.ttf"
 ODDS = .5
 FONT_SIZE = 30
 ODDS_OFFSET = .25
-VERTICAL_OFFSET = 15
+VERTICAL_OFFSET = 20
 TEXT_COLOR = (0, 0, 0)
+LINE_LENGTH_START = 30
 REGION_COLOR = (255, 255, 255)
 HORIZONTAL_OFFSET = VERTICAL_OFFSET
 
@@ -26,7 +31,7 @@ async def command_sponge(client, message):
     if command_match is None or not is_ascii(command_match.group("rest")):
         await command_sponge_help(client, message)
         return
-    
+
     # Get the normal and SpOnGe text from the raw text of the message.
     normal_text, sponge_text = generate_texts(message.author.display_name,
                                               command_match.group("rest"))
@@ -49,10 +54,10 @@ def is_ascii(text):
 
 
 def generate_texts(user, text):
-    """Create the normal and SpOnGeD text strings for the meme."""
+    """Create the normal and SpOnGe text strings for the meme."""
 
-    # Create a list of characters from the input text, but turn all
-    # whitespace into spaces.
+    # Create a list of characters from the input text, and turn all
+    # whitespace into regular spaces.
     characters = [" " if char.isspace() else char for char in text]
 
     # Force all characters to be lower case.
@@ -90,21 +95,44 @@ def generate_texts(user, text):
     return normal_text, sponge_text
 
 
+def wrap_text(text, canvas, width, font):
+    """Wrap text based on its max width, given in pixels for a specific font."""
+
+    # Start guessing at a certain constant character count.
+    character_count = LINE_LENGTH_START
+    usable_width = width - HORIZONTAL_OFFSET * 2
+
+    # Use a loop to find the best break point for the text for the given width.
+    # This is certainly not the most efficient way to do this.
+    copied_text = textwrap.fill(text, width=character_count)
+    while canvas.textsize(copied_text, font=font)[0] < usable_width:
+        character_count += 1
+        copied_text = textwrap.fill(text, width=character_count)
+
+    # Return the text, wrapped to the appropriate length (minus one to
+    # account for the extra iteration of the while loop).
+    return textwrap.fill(text, width=character_count - 1)
+
+
 def generate_image(normal_text, sponge_text):
-    """Generate a meme with provided normal and SpOnGeD text."""
+    """Generate a meme with provided normal and SpOnGe text."""
     font = ImageFont.truetype(FONT_FILE, FONT_SIZE)
 
     # Open the template and set up a dummy canvas for measurements.
     template = Image.open(TEMPLATE_FILE)
     template_width, template_height = template.size
-    dummy_canvas = ImageDraw.Draw(template)
+    test_canvas = ImageDraw.Draw(template)
 
-    # The clearance is the space between the normal text and the SpOnGeD text.
-    clearance = dummy_canvas.textsize("A", font=font)[1] * 2
-    normal_height = dummy_canvas.textsize(normal_text, font=font)[1]
-    sponge_height = dummy_canvas.textsize(sponge_text, font=font)[1]
+    # Wrap the normal and SpOnGe text for the image.
+    normal_text = wrap_text(normal_text, test_canvas, template_width, font)
+    sponge_text = wrap_text(sponge_text, test_canvas, template_width, font)
 
-    # Calculate the height of the necessary white text region above the template.
+    # The clearance is the space between the normal text and the SpOnGe text.
+    clearance = test_canvas.textsize("A", font=font)[1] * 2
+    normal_height = test_canvas.textsize(normal_text, font=font)[1]
+    sponge_height = test_canvas.textsize(sponge_text, font=font)[1]
+
+    # Calculate the height of the white text region above the template.
     text_region_height = (VERTICAL_OFFSET + normal_height +
                           clearance + sponge_height +
                           VERTICAL_OFFSET)
@@ -112,9 +140,9 @@ def generate_image(normal_text, sponge_text):
     # Create a new image with a white region above the template for text,
     # and prepare the canvas to draw the text.
     meme = template.crop((0, -text_region_height, template.width, template_height))
-    canvas = ImageDraw.Draw(meme)
 
     # Fill the text region with the appropriate color.
+    canvas = ImageDraw.Draw(meme)
     canvas.rectangle((0, 0, template_width, text_region_height), fill=REGION_COLOR)
 
     # Calculate the topleft coordinates for both strings of text.
