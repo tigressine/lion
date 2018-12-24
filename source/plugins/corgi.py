4d64642f70dc18a5feb74518aa951a0d6fe9b323
Tiger Sachse <tiger@sachse.io>
2018-12-24 10:27:18 -0500

    working on reworking corgi, not finished

--- source/plugins/corgi.py
+++ source/plugins/corgi.py
@@ -1,50 +1,64 @@
+"""Post a random corgi picture.
+
+Written by Hayden Inghem and Joey the Corgi.
+Revised by Tiger Sachse.
 """
-Written by Hayden Inghem and Joey The corgi 
-"""
-import discord
-import aiohttp
-import random
 import json
+import random
+#import aiohttp
+import discord
+import requests
 
 COMMAND = "corgi"
+TUMBLR_KEY = "m9ZzEYfFoBEjEQpjqYunya8Ji2802GMj1ng1MWbKVY7Ra8kGEP"
+TEMPORARY_FILE_FORMAT = "/tmp/random_corgi.{0}"
+CORGI_BLOGS = (
+    "corgito",
+    "acorgiaday",
+    "worldofcorgi",
+    "corgioverload",
+    "vegasthecorgi",
+    "discothecorgi",
+    "omelettethecorgi",
+)
 
-# tumblr key
-key = "m9ZzEYfFoBEjEQpjqYunya8Ji2802GMj1ng1MWbKVY7Ra8kGEP"
+async def command_corgi(client, message):
+    """Fetch a corgi picture from Tumblr and post it."""
+    blog_url = (
+        "https://api.tumblr.com/v2/blog/"
+        + random.choice(CORGI_BLOGS)
+        + "/posts/photo?api_key="
+        + TUMBLR_KEY
+    )
 
-# If u have anymore strictly corgi blogs pls add
-
-corgi_blogs = [
-        "corgioverload",
-        "acorgiaday",
-        "omelettethecorgi",
-        "discothecorgi",
-        "worldofcorgi",
-        "corgito",
-        "vegasthecorgi"
-        ]
-
-
-
-async def command_corgi(client,message):
+    # First, attempt to retrieve a random corgi image URL from one of the blogs.
     try:
-        session = aiohttp.ClientSession()
-        random_element = random.randrange(0, len(corgi_blogs))
-        async with session.get("https://api.tumblr.com/v2/blog/" + corgi_blogs[random_element] + "/posts/photo?api_key=" + key) as r:
-            if r.status == 200:
-                response = await r.read()
-                data = json.loads(response)
-                random_picture = random.randrange(0, 20)
-                image_url = data['response']['posts'][random_picture]['photos'][0]['original_size']['url']
-            await client.send_message(message.channel, image_url)
-            session.close()
-    except Exception as e:
-        session.close()
-        print("Exception: %s" % e)
-        await command_corgi(client,message)
+        image_url_request = requests.get(blog_url)
+        if image_url_request.status_code == 200:
+            image_url = get_random_image_url(image_url_request.json())
+        else:
+            raise requests.RequestException("URL request status was not 200.")
+
+        # Next, attempt to download that random image.
+        image_request = requests.get(image_url)
+        if image_request.status_code == 200:
+            with open(TEMPORARY_FILE, "wb") as writable_file:
+                writable_file.write(image_request.content)
+        else:
+            raise requests.RequestException("Image request status was not 200.")
+
+        # 
+
+        # Finally, send the image to the client.
+        await client.send_file(message.channel, TEMPORARY_FILE)
+
+    except Exception as exception:
+        print(exception)
+        await client.send_message(message.channel, "Something wen't wrong!")
 
 
+def get_random_image_url(data):
+    """Get a random image URL from the data dictionaries."""
+    random_post = random.choice(data["response"]["posts"])
 
-
-
-
-
+    return random_post["photos"][0]["original_size"]["url"]
