b97b7afce580390c22baf605fbb94d2a20fcba72
Tiger Sachse <tgsachse@gmail.com>
2018-12-26 22:11:27 -0500

    delete messages using link filter

--- source/plugins/require_links.py
+++ source/plugins/require_links.py
@@ -3,10 +3,25 @@
 Written by Tiger Sachse.
 """
 
+import re
+
+URL_FORMAT = "(?P<url>https?://\S+)"
 FILTERED_CHANNELS = frozenset(("bot_testing", "personal_projects", "helpful_baubles"))
+NO_LINK_WARNING_FORMAT = (
+    "The channel #{0} is a repository for links, not a place for discussion. "
+    + "Any post in this channel must include a valid link, but yours did not."
+    + "\n\nYour original post:\n```{1}```"
+)
 
 async def filter_require_links(client, message):
     """Ensure that any messages in a filtered channel contain links."""
 
-    print(message.embeds)
-    await client.send_message(message.channel, "filtered chan")
+    search = re.search(URL_FORMAT, message.content)
+    if search is None:
+        await client.delete_message(message) 
+        await client.send_message(
+            message.author,
+            NO_LINK_WARNING_FORMAT.format(message.channel, message.content)
+        )
+    else:
+        await client.send_message(message.channel, "noice")
