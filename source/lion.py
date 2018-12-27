4405051fe552c74bb87c9e6f7439057930700f8b
root <root@ns558046.ip-192-99-5.net>
2018-12-27 12:01:59 -0500

    create dog.py

--- source/lion.py
+++ source/lion.py
@@ -54,10 +54,11 @@ async def on_message(message):
 
 def load_token():
     """Load Lion's token from a json file."""
-    with open(TOKEN_FILE, "r") as token_file:
-        token = json.load(token_file)
+    # with open(TOKEN_FILE, "r") as token_file:
+    #     token = json.load(token_file)
 
-    return token
+    #return token
+    return 'NTI3ODYxMDQwNjI5ODA5MTUz.DwaVFw.0nTG9hB94T1d_tIVYZrmkWHRYC0'
 
 
 def handle_signals(signal_number, stack_frame):
