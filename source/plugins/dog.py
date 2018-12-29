9cb4647ed99d22dcb3143e49962e90d22970fe72
joey <odus@knights.ucf.edu>
2018-12-28 19:01:54 -0500

    update dog.py to send dog pictures through local file

--- source/plugins/dog.py
+++ source/plugins/dog.py
@@ -13,8 +13,24 @@ COMMAND = "dog"
 
 API_URL = "https://api.woofbot.io/v1/"
 API_DOG_URL_FORMAT = "https://api.woofbot.io/v1/breeds/{0}/image"
+IMAGE_FILE = "/tmp/dog.jpg"
 
-async def fetch_url(url):
+async def fetch_image(url):
+    """ Fetches image from a URL. If successful, returns image data, else returns empty string """
+    # Initialize response to be empty
+    response = ''
+    try:
+        session = aiohttp.ClientSession()
+        async with session.get(url) as url_response:
+            if url_response.status == 200:
+                response = await url_response.read()
+    finally:
+        session.close()
+        
+    return response
+
+async def fetch_json(url):
+    """ Fetches json from a URL. If successful, returns json object, else returns empty string """
     # Initialize response to be empty
     response = ''
     try:
@@ -44,23 +60,36 @@ async def command_dog(client, message):
     
 
     try:
-        web_response = await fetch_url(request_url)
+        web_response = await fetch_json(request_url)
 
         # Do we have data within web_response?
         if web_response != '':
-            response = web_response["response"]["url"]
+            image_url = web_response["response"]["url"]
+            image_contents = await fetch_image(image_url)
+
+            # We need to make sure image_contents actually contains an image.
+            if image_contents != "":
+                with open(IMAGE_FILE, "wb") as writable_file:
+                    writable_file.write(image_contents)
+                    await client.send_file(message.channel, IMAGE_FILE)
+                
         else:
             response = "%s are currently not supported 😓... Type `!dog breeds` for a list of breeds the bot currently supports!" % (breed)
+    
+    except Exception:
+        response = "Something went wrong 😭"
 
     finally:
-        await client.send_message(message.channel, response)
-
+        # Check if we have a response. We won't have a response in the event that
+        # everything successfully runs.
+        if response != "":
+            await client.send_message(message.channel, response)
 
 async def get_breeds():
     """ Grabs list of breeds that woofbot.io currently supports and formats into a friendly message """
     try:
         url = API_URL.__add__("breeds")
-        web_response = await fetch_url(url)
+        web_response = await fetch_json(url)
 
         # Do we have data within web_response?
         if web_response != '':
