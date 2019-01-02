b222e30f3086e4ece3e5857b7bf067c482873dcc
Tiger Sachse <tgsachse@gmail.com>
2019-01-01 21:42:40 -0500

    reworked egg

--- source/plugins/egg.py
+++ source/plugins/egg.py
@@ -1,28 +1,26 @@
-"""Displays the original users message in regional letter indicators.
-Inspired by an incident between *NIX and eggplantpatrol.
+"""Displays the original users message with regional letter indicators.
 
-Written by Caleb-Shepard and kobeeraveendran.
-Some data structure revision by Khalil Wehmeyer.
-Small updates by Tiger Sachse.
+Inspired by an incident between *NIX and eggplantpatrol.
+Written by Caleb-Shepard, kobeeraveendran, Khalil Wehmeyer, and Tiger Sachse.
 """
 import re
-import nltk
 
 COMMAND = "egg"
+CHARACTER_LIMIT = 2000
+RESPONSE_FORMAT = "{0}:\n{1}"
 COMMAND_FORMAT = r"^!{0} (?P<rest>.*)$".format(COMMAND)
-MAX_MESSAGE_LENGTH = 1000
 EGG_DICT = {
-    '1': ':one:', 
-    '2': ':two:',
-    '3': ':three:',
-    '4': ':four:',
-    '5': ':five:',
-    '6': ':six:',
-    '7': ':seven:',
-    '8': ':eight:',
-    '9': ':nine:',
-    '0': ':zero:',
-    '!': ':exclamation:'
+    '1': ':one: ', 
+    '2': ':two: ',
+    '3': ':three: ',
+    '4': ':four: ',
+    '5': ':five: ',
+    '6': ':six: ',
+    '7': ':seven: ',
+    '8': ':eight: ',
+    '9': ':nine: ',
+    '0': ':zero: ',
+    '!': ':exclamation: '
 }
 
 def is_ascii(text):
@@ -42,54 +40,26 @@ async def command_egg(client, message):
 
         return
 
-    # Figure out who sent the command.
-    message_author_display_name = message.author.display_name
-    message_body = command_match.group("rest")
+    # Save all characters and digits in a components list as regional indicators.
+    response_components = []
+    for char in command_match.group("rest"):
+        if char.isspace():
+            response_components.append("  ")
+        elif char.isalpha():
+            response_components.append(
+                ":regional_indicator_{0}: ".format(char.lower())
+            )
+        elif char in EGG_DICT:
+            response_components.append(EGG_DICT[char])
 
-    # Begin generating our response to the sender; this will be a list of words
-    # rather than a string to simplify, beautify, and optimize message splits
-    # when the character limit is surpassed. Messages will be split by word
-    # and never mid-word unless if absolutely neccessary.
-    full_response = []
-    response = ""
-    tokenized_message = nltk.word_tokenize(message_body)
+    # Compress the response components into a string, and add a mention.
+    mention = message.author.mention
+    response_component_string = "".join(response_components)
+    response = RESPONSE_FORMAT.format(mention, response_component_string)
 
-    # Dictionary remapping of individual chatacters to their egg representation.
-    for word in tokenized_message:
-
-        # Convert a word into its egg representation.
-        egg_word = ""
-        for char in word:
-            if not is_ascii(char):
-                pass
-            elif char.isalpha():
-                egg_word += ":regional_indicator_" + char.lower() + ": "
-            elif char in EGG_DICT:
-                egg_word += EGG_DICT[char] + ' '
-
-        if len(egg_word) + len(response) < MAX_MESSAGE_LENGTH:
-            response += egg_word
-        else:
-
-            # Save one (1000 character or less) section of the message
-            # so that the message may be split into different parts.
-            full_response.append(response)
-
-            # Reset the value of "response" and continue.
-            response = ""
-
-        response += "   "
-
-    # If the sender signature fits in the resulting message without overflowing
-    # to a new message, then add it to the end. Otherwise just send it separately.
-    if len(response + " - " + message_author_display_name) < MAX_MESSAGE_LENGTH:
-        response += " - " + message_author_display_name
-        full_response.append(response)
-    else:
-        full_response.append(" - " + message_author_display_name)
-
-
-    # There may be more than one message to send, so loop through the full response
-    # and send each message sequentially.
-    for response in full_response:
+    # Send the mention and response to the client.
+    if len(response) < CHARACTER_LIMIT:
         await client.send_message(message.channel, response)
+    else:
+        sassy_response = "I can't handle that many characters. You gotta chill fam."
+        await client.send_message(message.channel, sassy_response)
