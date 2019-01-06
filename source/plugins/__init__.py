52a33f791f09fa847830641fb365677b6f667c77
Sam <46390361+samoshan@users.noreply.github.com>
2019-01-06 17:03:15 -0500

    added weather plugin

--- source/plugins/__init__.py
+++ source/plugins/__init__.py
@@ -18,6 +18,7 @@ from plugins import (
     user_count,
     garage_status,
     require_links,
+    weather,
 )
 
 FILTERED_CHANNELS = {
@@ -36,6 +37,7 @@ COMMANDS = {
     roles.REMOVE_COMMAND : roles.command_removeroles,
     user_count.COMMAND : user_count.command_user_count,
     garage_status.COMMAND : garage_status.command_garage_status,
+    weather.COMMAND : weather.command_weather,
 }
 
 INLINES = {
