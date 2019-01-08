390e79a853d3e4f67c6d000b50bdc8a72a228f64
Tiger Sachse <tgsachse@gmail.com>
2019-01-08 15:01:22 +0000

    removed requirement that links be valid in require_links

--- source/plugins/require_links.py
+++ source/plugins/require_links.py
@@ -1,4 +1,4 @@
-"""Require that all posts in filtered channels contain valid hyperlinks.
+"""Require that all posts in filtered channels contain hyperlinks.
 
 This is useful for reducing/eliminating conversation in channels that link to
 personal projects or helpful information.
@@ -7,18 +7,12 @@ Written by Tiger Sachse.
 """
 
 import re
-import ssl
-import httplib2
 
 URL_FORMAT = "(?P<url>https?://\S+)"
 FILTERED_CHANNELS = frozenset(("personal_projects", "helpful_baubles"))
-NO_LINK_WARNING_FORMAT = (
+WARNING_FORMAT = (
     "The channel #{0} is a repository for links, not a place for discussion. "
     + "Any post in this channel must include a valid link, but yours did not."
-    + "\n\nYour original post:\n```{1}```"
-)
-INVALID_LINK_WARNING_FORMAT = (
-    "All posts in #{0} must contain active links. Your post's link was invalid."
     + "\n\nYour original post:\n```{1}```"
 )
 
@@ -31,26 +25,5 @@ async def filter_require_links(client, message):
         await client.delete_message(message) 
         await client.send_message(
             message.author,
-            NO_LINK_WARNING_FORMAT.format(message.channel, message.content)
+            WARNING_FORMAT.format(message.channel, message.content)
         )
-
-    # Otherwise, check the validity of the link. If it is invalid, delete
-    # the new message and DM the author.
-    elif is_invalid(search.group("url")):
-        await client.delete_message(message)
-        await client.send_message(
-            message.author,
-            INVALID_LINK_WARNING_FORMAT.format(
-                message.channel,
-                message.content
-            )
-        )
-
-def is_invalid(link):
-    """Check if a link is invalid by requesting its header."""
-    try:
-        response = httplib2.Http().request(link, "HEAD")
-    except (httplib2.HttpLib2Error, ssl.SSLError):
-        return True
-    
-    return False if int(response[0]["status"]) < 400 else True
