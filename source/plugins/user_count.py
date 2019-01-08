87e04d6835cc88e2a0c94fa273a808b30fc04901
Tiger Sachse <tgsachse@gmail.com>
2019-01-07 19:24:33 -0500

    #32 improved user count

--- source/plugins/user_count.py
+++ source/plugins/user_count.py
@@ -1,11 +1,20 @@
 """Displays the server's user count.
 
 Written by Tiger Sachse.
+Inspired by Discord-User-Counter by Eric Woolard.
 """
 COMMAND = "users"
+RESPONSE_FORMAT = "**This server has {0} members ({1} currently online).**"
 
 async def command_user_count(client, message):
     """Display the server's user count."""
     server = next(iter(client.servers))
-    response = "**This server has {0} members.**".format(server.member_count)
+
+    active_members = 0
+    for member in server.members:
+        status = str(member.status)
+        if status == "online" or status == "idle":
+            active_members += 1
+
+    response = RESPONSE_FORMAT.format(server.member_count, active_members)
     await client.send_message(message.channel, response)
