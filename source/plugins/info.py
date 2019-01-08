47463c26d2ee8a4a42130a088c1d5fdd33104a3d
Tiger Sachse <tgsachse@gmail.com>
2019-01-08 15:02:16 +0000

    hotfix 1.2.1

--- source/plugins/info.py
+++ source/plugins/info.py
@@ -32,7 +32,7 @@ async def command_info(client, message):
     
     embedded_message.add_field(
         name="Version",
-        value="1.2.0",
+        value="1.2.1",
         inline=False
     )
 
