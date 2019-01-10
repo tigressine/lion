ee996c61bf805ee9e9422bbc372fd7bb1356382c
Matt Bautista <nayoshi12@gmail.com>
2019-01-10 02:10:11 -0500

    Created Magic Ball plugin and documentation prep

---
+++ source/plugins/README.md
@@ -0,0 +1,3 @@
+# Creating a plugin
+## To do
+* Create documentation on how to create plugins cuz its too late to do now
---
+++ source/plugins/magicball.py
@@ -0,0 +1,36 @@
+import json
+import random
+import discord
+import re
+COMMAND = "magicball"
+
+COMMAND_PATTERN = r"^\!{0}( (?P<question>[a-zA-Z ]([^\"]+)+))?$".format(COMMAND)
+"""This prints out the messages to see if the json is loaded"""
+debug = False
+async def command_magicball(client,message):
+    """See if !magic (message) matches the regex pattern above.
+    This also allows for easy string extraction from message"""
+    command_match = re.match(COMMAND_PATTERN, message.content)
+
+
+    response = command_match.group("question")
+
+    if response is None:
+        response = "Hmmmmm"
+    """JSON to a 2d array"""
+    with open("data/8ball_responses.json") as f:
+        data = json.load(f)
+
+    if debug:
+        print(data["magicball"][random.randint(0, len(data["magicball"]))])
+
+    """Embedded Discord message creation"""
+    embed = discord.Embed(color=0x6105ff)
+
+    embed.set_author(
+        name="Magic Ball of Fortune", icon_url = "https://cdn.discordapp.com/attachments/353269187633741824/532808475781234688/61F8HIGkUiL.png")
+
+    embed.add_field(name=response, value=data["magicball"][random.randint(0, len(data["magicball"]))], inline=False)
+
+    await message.channel.send(embed=embed)
+
