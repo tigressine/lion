f7c12ec2f1f82eca5d3de82f64bf28e8349c6ab6
Tiger Sachse <tiger@sachse.io>
2019-02-08 14:57:33 -0500

    #71 example plugin finished

--- lion/cogs/example/example.py
+++ lion/cogs/example/example.py
@@ -1,26 +1,59 @@
+"""An example cog for educational purposes.
+
+This cog creates a new command called 'example' which takes 1 argument: a
+number. This cog loads a text file from 'assets/' based on the given number and
+then posts the contents of that file to the user's server.
+
+Written by Tiger Sachse.
+"""
+
+# The utilities module provides necessary functions like respond() and
+# throw_error(). Use these functions to interact with the Discord API, instead
+# of the functions provided directly by the API.
 import utilities
 from discord.ext import commands
 
+
 def setup(bot):
+    """Add this cog to a given bot.
+
+    Every cog must include this function.
+    """
     bot.add_cog(ExampleCog())
 
 
 class ExampleCog:
+    """This class contains all the logic for the 'example' command."""
+
+    # Notice that paths for assets must be relative and must start at the
+    # 'cogs' directory. If you try to access 'assets/example{0}.txt' the program
+    # will not understand because of how Python keeps track of relative paths.
     EXAMPLE_PATH_FORMAT = "cogs/example/assets/example{0}.txt"
     
     @commands.command(name="example")
     async def example_command(self, context, number: int):
-        """"""
+        """Main entry point for the example command."""
+
+        # Do not handle errors directly in your cog_command() function. Raise
+        # the appropriate exception and let the cog_error() function handle it.
         if number < 1 or number > 3:
             raise commands.errors.BadArgument("Number out of range.")
+
+        # Use the respond() function to send text to the user's server. This
+        # function accepts all the same arguments that context.send() accepts.
         await utilities.respond(context, self.get_example_text(number))
 
     @example_command.error
-    async def on_example_error(self, context, error):
-        """"""
+    async def example_error(self, context, error):
+        """Handle any errors that occur during the 'example' command.
+        
+        This function passes the error and the context to the throw_error()
+        function which outputs the error to the user's server. You can add code
+        here to ignore certain errors or do other, more complicated things.
+        """
         await utilities.throw_error(context, error)
 
     def get_example_text(self, number):
-        """"""
+        """Load text from a file."""
         with open(self.EXAMPLE_PATH_FORMAT.format(number), "r") as example_file:
             return example_file.read()
--- lion/utilities.py
+++ lion/utilities.py
@@ -1,17 +1,22 @@
-import logging
+"""A collection of utility functions for Lion.
 
+Written by Tiger Sachse.
+"""
 async def throw_error(context, error, message=None):
-    """"""
+    """Send an error to the context's server.
+    
+    If a message is provided, that message is included in the post, otherwise
+    the error's default message is included.
+    """
     ERROR_FORMAT = ("Something went wrong!\n```\n"
                     "Command : {0}\n"
                     "Error   : {1}\n"
-                    "Message : {2}\n"
-                    "```")
+                    "Message : {2}\n```")
 
     response = ERROR_FORMAT.format(context.command,
                                    type(error).__name__,
                                    message if message is not None else error)
-    await context.send(response)
+    await respond(context, response)
 
 
 async def respond(context,
@@ -19,9 +24,14 @@ async def respond(context,
                   ignore_formatting=False,
                   delete_original=True,
                   **keyword_arguments):
-    """"""
+    """Respond to a message in context.
+    
+    The response is automatically formatted with the original message's author's
+    handle, although this can be disabled with the ignore_formatting flag.
+    """
     RESPONSE_FORMAT = "{0}:\n{1}"
 
+    # Avoid additionally formatting, if requested.
     if ignore_formatting:
         response = message
     else:
@@ -29,11 +39,12 @@ async def respond(context,
 
     await context.send(response, **keyword_arguments)
 
+    # Delete the original message by default.
     if delete_original:
         await context.message.delete()
 
 
 def load_token(token_name):
-    """Load the Discord API authentication token."""
+    """Load an API token from file."""
     with open(token_name, "r") as token_file:
         return token_file.read().strip()
