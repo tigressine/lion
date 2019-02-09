6286ed67640c06f641e694f45c7ce9fb4769c86d
Tiger Sachse <tiger@sachse.io>
2019-02-09 15:06:19 -0500

    #30 #67 logs work (although it's hacky). Moving CLI #62 into a cog

--- lion/liond.py
+++ lion/liond.py
@@ -5,6 +5,7 @@ run continously until interrupted. This script should be run as a daemon.
 
 Written by Tiger Sachse.
 """
+import logging
 import settings
 import utilities
 from pathlib import Path
@@ -13,24 +14,28 @@ from discord.ext import commands
 
 def load_cogs(bot, cogs_directory):
     """Load all cogs within a cogs directory into a bot."""
+    LOG_MESSAGE_FORMAT = "Attempting to load '{0}' cog: {1}"
+
     cogs_loaded = 0
     cogs_path = Path(cogs_directory)
-
     for cog in (path.stem for path in cogs_path.iterdir()):
-        print("Attempting to load '{0}' cog:".format(cog), end="")
-        
         try:
             bot.load_extension(".".join((cogs_directory, str(cog), str(cog))))
-            print(" Success!")
+            utilities.log(LOG_MESSAGE_FORMAT.format(cog, " Success!"),
+                          level=logging.INFO)
             cogs_loaded += 1
         except ModuleNotFoundError:
-            print(" Failure!")
+            utilities.log(LOG_MESSAGE_FORMAT.format(cog, " Failure!"),
+                          level=logging.WARNING)
 
-    print("Loaded {0} cog{1}.".format(cogs_loaded,
-                                      "s" if cogs_loaded != 1 else ""))
+    loaded_message = "Loaded {0} cog{1}.".format(cogs_loaded,
+                                                 "s" if cogs_loaded != 1 else "")
+    utilities.log(loaded_message, level=logging.INFO)
 
 
 # Main entry point to the daemon.
+utilities.log("Initializing Lion...", level=logging.INFO)
 bot = commands.Bot(command_prefix=settings.PREFIX)
 load_cogs(bot, settings.COGS_DIRECTORY)
+utilities.log("Starting Lion...", level=logging.INFO)
 bot.run(utilities.load_token(settings.TOKEN_FILE))
