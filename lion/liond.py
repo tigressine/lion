0843d2a81bea581c5bbfdc2fbfde7c045c0e09b7
Tiger Sachse <tiger@sachse.io>
2019-02-11 00:31:31 -0500

    #82 everything is inside the bot now

--- lion/liond.py
+++ lion/liond.py
@@ -11,60 +11,75 @@ import settings
 import utilities
 from pathlib import Path
 from discord.ext import commands
+from systemd.journal import JournalHandler
 
 
 class LionBot(commands.Bot):
     """"""
-
-    def __init__(self, guild_settings_file, *arguments, **keyword_arguments):
+    def __init__(self,
+                 guild_settings_file,
+                 cogs_directory,
+                 *arguments,
+                 **keyword_arguments):
         """"""
         super().__init__(*arguments, **keyword_arguments)
 
-        self.guild_settings_file = guild_settings_file
+        self._logger = logging.getLogger(__name__)
+        self._logger.addHandler(JournalHandler())
+        self._logger.setLevel(logging.INFO)
+
+        self.load_cogs(cogs_directory)
+
+        self._guild_settings_file = guild_settings_file
         try:
-            with open(self.guild_settings_file, "r") as open_guild_settings_file:
-                self.guild_settings = json.load(open_guild_settings_file)
+            with open(self._guild_settings_file, "r") as open_guild_settings_file:
+                self._guild_settings = json.load(open_guild_settings_file)
         except FileNotFoundError:
-            self.guild_settings = {}
+            self._guild_settings = {}
 
     def write_guild_settings(self):
         """"""
-        with open(self.guild_settings_file, "w") as open_guild_settings_file:
-            json.dump(open_guild_settings_file, self.guild_settings)
-
+        with open(self._guild_settings_file, "w") as open_guild_settings_file:
+            json.dump(open_guild_settings_file, self._guild_settings)
 
     def get_guild_settings(self, guild_id):
-        return self.guild_settings[str(guild_id)]
-    '''
+        """"""
+        guild_id = str(guild_id)
+        if guild_id in self._guild_settings:
+            return self._guild_settings[guild_id]
+        else:
+            return None
+    
     def log(self, message, level=logging.INFO):
-        pass
-    '''
+        """"""
+        self._logger.log(level, message)
 
+    def load_cogs(self, cogs_directory):
+        """Load all cogs within the cogs directory into the bot."""
+        LOG_MESSAGE_FORMAT = "Attempting to load '{0}' cog: {1}"
 
-def load_cogs(bot, cogs_directory):
-    """Load all cogs within a cogs directory into a bot."""
-    LOG_MESSAGE_FORMAT = "Attempting to load '{0}' cog: {1}"
+        cogs_loaded = 0
+        cogs_directory = Path(cogs_directory)
+        for cog in (path.stem for path in cogs_directory.iterdir()):
+            try:
+                self.load_extension(".".join((str(cogs_directory),
+                                              str(cog),
+                                              str(cog))))
+                self.log(LOG_MESSAGE_FORMAT.format(cog, " Success!"),
+                         level=logging.INFO)
+                cogs_loaded += 1
+            except ModuleNotFoundError:
+                self.log(LOG_MESSAGE_FORMAT.format(cog, " Failure!"),
+                         level=logging.WARNING)
 
-    cogs_loaded = 0
-    cogs_path = Path(cogs_directory)
-    for cog in (path.stem for path in cogs_path.iterdir()):
-        try:
-            bot.load_extension(".".join((cogs_directory, str(cog), str(cog))))
-            utilities.log(LOG_MESSAGE_FORMAT.format(cog, " Success!"),
-                          level=logging.INFO)
-            cogs_loaded += 1
-        except ModuleNotFoundError:
-            utilities.log(LOG_MESSAGE_FORMAT.format(cog, " Failure!"),
-                          level=logging.WARNING)
-
-    loaded_message = "Loaded {0} cog{1}.".format(cogs_loaded,
-                                                 "s" if cogs_loaded != 1 else "")
-    utilities.log(loaded_message, level=logging.INFO)
+        log_message = "Loaded {0} cog{1}.".format(cogs_loaded,
+                                                  "s" if cogs_loaded != 1 else "")
+        self.log(log_message, level=logging.INFO)
 
 
 # Main entry point to the daemon.
-utilities.log("Initializing Lion...", level=logging.INFO)
-bot = LionBot(settings.GUILD_SETTINGS_PATH, command_prefix=settings.PREFIX)
-load_cogs(bot, settings.COGS_DIRECTORY)
-utilities.log("Starting Lion...", level=logging.INFO)
+bot = LionBot(settings.GUILD_SETTINGS_PATH,
+              settings.COGS_DIRECTORY,
+              command_prefix=settings.PREFIX)
+bot.log("Starting Lion...", level=logging.INFO)
 bot.run(utilities.load_token(settings.TOKEN_PATH))
