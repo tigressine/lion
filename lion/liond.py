321b7eb023657fb2b62bd6fdeccbff271f6e54b6
Tiger Sachse <tiger@sachse.io>
2019-02-11 00:42:33 -0500

    comment drive, finalized LionBot class pre-cog loading rework

--- lion/liond.py
+++ lion/liond.py
@@ -15,44 +15,34 @@ from systemd.journal import JournalHandler
 
 
 class LionBot(commands.Bot):
-    """"""
+    """A custom child class of the discord.py API's built-in Bot class.
+
+    This class maintains a logger and a dictionary of guild-specific settings.
+    """
     def __init__(self,
                  guild_settings_file,
                  cogs_directory,
                  *arguments,
                  **keyword_arguments):
-        """"""
+        """Initialize the bot with a logger and a dictionary of guild settings."""
         super().__init__(*arguments, **keyword_arguments)
 
+        # Create a new logger.
         self._logger = logging.getLogger(__name__)
         self._logger.addHandler(JournalHandler())
         self._logger.setLevel(logging.INFO)
 
+        # Load all cogs in the cogs directory.
         self.load_cogs(cogs_directory)
 
+        # Load the guild settings from the guild settings file. If the file
+        # doesn't exist, create a new empty dictionary.
         self._guild_settings_file = guild_settings_file
         try:
             with open(self._guild_settings_file, "r") as open_guild_settings_file:
                 self._guild_settings = json.load(open_guild_settings_file)
         except FileNotFoundError:
             self._guild_settings = {}
-
-    def write_guild_settings(self):
-        """"""
-        with open(self._guild_settings_file, "w") as open_guild_settings_file:
-            json.dump(open_guild_settings_file, self._guild_settings)
-
-    def get_guild_settings(self, guild_id):
-        """"""
-        guild_id = str(guild_id)
-        if guild_id in self._guild_settings:
-            return self._guild_settings[guild_id]
-        else:
-            return None
-    
-    def log(self, message, level=logging.INFO):
-        """"""
-        self._logger.log(level, message)
 
     def load_cogs(self, cogs_directory):
         """Load all cogs within the cogs directory into the bot."""
@@ -75,6 +65,23 @@ class LionBot(commands.Bot):
         log_message = "Loaded {0} cog{1}.".format(cogs_loaded,
                                                   "s" if cogs_loaded != 1 else "")
         self.log(log_message, level=logging.INFO)
+
+    def write_guild_settings(self):
+        """Write the current guild settings to disk."""
+        with open(self._guild_settings_file, "w") as open_guild_settings_file:
+            json.dump(open_guild_settings_file, self._guild_settings)
+
+    def get_guild_settings(self, guild_id):
+        """Get a particular guild's settings, based on the guild id."""
+        guild_id = str(guild_id)
+        if guild_id in self._guild_settings:
+            return self._guild_settings[guild_id]
+        else:
+            return None
+    
+    def log(self, message, level=logging.INFO):
+        """Log a message using the bot's logger."""
+        self._logger.log(level, message)
 
 
 # Main entry point to the daemon.
