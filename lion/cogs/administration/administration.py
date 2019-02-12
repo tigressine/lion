aad74fc8632911f72abb8b7e3bdeed89e34e520f
Tiger Sachse <tiger@sachse.io>
2019-02-11 21:15:09 -0500

    comment drive, close #63

--- lion/cogs/administration/administration.py
+++ lion/cogs/administration/administration.py
@@ -1,4 +1,5 @@
-"""
+"""An administration cog for delicate functions.
+
 Written by Tiger Sachse.
 """
 import utilities
@@ -11,11 +12,11 @@ def setup(bot):
 
 
 class AdministrationCog:
-    """"""
+    """This class contains all the logic for the 'admin' command."""
     @commands.command(name="admin")
     @commands.check(utilities.is_administrator)
     async def admin_command(self, context, flag):
-        """"""
+        """This command takes a flag as its argument and acts accordingly."""
         if flag == "--default-channel":
             context.bot.set_guild_setting(context.guild.id,
                                           "default_channel",
@@ -28,7 +29,7 @@ class AdministrationCog:
 
     @admin_command.error
     async def admin_error(self, context, error):
-        """"""
+        """Handle any errors that occurred during the 'admin' command."""
         if isinstance(error, commands.errors.CheckFailure):
             message = "You must be an administrator to run this command."
             await utilities.throw_error(context, error, message)
